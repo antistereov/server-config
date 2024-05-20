f2afa72c7339fa94750277ef1ccf3ec6b8f00c9d
Stereov <admin@stereov.com>
2024-05-20 02:29:39 +0000

    Add timestamp to dyndns script

--- homeassistant/dyndns.sh
+++ homeassistant/dyndns.sh
@@ -12,7 +12,7 @@ WG_CONFIG_PATH="./wg0.conf"
 
 # Check if VPN_ENDPOINT_DOMAIN is set
 if [ -z "$VPN_ENDPOINT_DOMAIN" ]; then
-  echo "VPN_ENDPOINT_DOMAIN is not set in the .env file."
+  echo "$(date +"%m/%d/%Y %H:%M:%S"): VPN_ENDPOINT_DOMAIN is not set in the .env file."
   exit 1
 fi
 
@@ -20,7 +20,7 @@ fi
 NEW_IP=$(dig +short "$VPN_ENDPOINT_DOMAIN" | tail -n1)
 
 if [ -z "$NEW_IP" ]; then
-  echo "Failed to resolve IP for domain: $VPN_ENDPOINT_DOMAIN"
+  echo "$(date +"%m/%d/%Y %H:%M:%S"): Failed to resolve IP for domain: $VPN_ENDPOINT_DOMAIN"
   exit 1
 fi
 
@@ -29,14 +29,14 @@ CURRENT_IP=$(grep -Eo "Endpoint = [^:]*" "$WG_CONFIG_PATH" | awk '{print $3}')
 
 # Update wg0.conf only if the IP has changed
 if [ "$CURRENT_IP" != "$NEW_IP" ]; then
-  echo "Updating wg0.conf Endpoint from $CURRENT_IP to $NEW_IP"
+  echo "$(date +"%m/%d/%Y %H:%M:%S"): Updating wg0.conf Endpoint from $CURRENT_IP to $NEW_IP"
   sed -i -e "s/Endpoint = $CURRENT_IP:57168/Endpoint = $NEW_IP:57168/" "$WG_CONFIG_PATH"
 
   # Restart Docker Compose services
   docker compose down && docker compose up -d
 else
-  echo "wg0.conf Endpoint is already up to date."
+  echo "$(date +"%m/%d/%Y %H:%M:%S"): wg0.conf Endpoint is already up to date."
 fi
 
-echo "Script completed."
+echo "$(date +"%m/%d/%Y %H:%M:%S"): Script completed."
 
