a048f4cfb601a49fa8154a32749b605ecdbfdb0a
Stereov <admin@stereov.com>
2024-06-28 19:16:58 +0000

    Update backup script to only backup volumes and change dir accordingly

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Change this location based on your setup
-backup_parent_dir="/backup/STEREOV-SERVER/backup/docker"
+system_backup_dir="/mnt/vps-server-backup"
+docker_vol_backup_parent_dir="/mnt/docker-vol-backup"
 
 echo ""
 echo "$(date +"%m/%d/%Y %H:%M:%S"): Starting backup script."
@@ -21,30 +22,7 @@ get_last_three_months() {
 }
 
 # Define backup directories
-backup_dir="$backup_parent_dir/$(date +'%Y-%m-%d')"
-backup_volume_dir="$backup_dir/volumes"
-backup_container_dir="$backup_dir/containers"
-
-# Create backup directories if they don't exist
-mkdir -p "$backup_dir"
-mkdir -p "$backup_volume_dir"
-mkdir -p "$backup_container_dir"
-
-# Backup running containers
-echo "$(date +"%m/%d/%Y %H:%M:%S"): Backing up running containers..."
-running_containers=$(docker ps --quiet)
-for container_id in $running_containers; do
-    container_name=$(docker inspect --format '{{.Name}}' "$container_id" | cut -c2-)
-    container_backup_archive="$backup_container_dir/$container_name.tar"
-
-
-    # Export the container filesystem to a tarball
-    docker export "$container_id" > "$container_backup_archive"
-
-    echo "$(date +"%m/%d/%Y %H:%M:%S"): Backup of container '$container_name' completed."
-done
-echo "$(date +"%m/%d/%Y %H:%M:%S"): All running containers backed up to '$backup_container_dir'."
-
+backup_dir="$docker_vol_backup_parent_dir/$(date +'%Y-%m-%d')"
 
 # Backup volumes
 echo "$(date +"%m/%d/%Y %H:%M:%S"): Backing up volumes..."
@@ -53,20 +31,20 @@ for volume in $volumes; do
     volume_name=$(docker volume inspect --format '{{.Name}}' "$volume")
 
     # Backup the volume by copying its contents to the backup directory
-    docker run --rm -v "$volume":/volume -v "$backup_volume_dir":/backup ubuntu tar cvf "backup/$volume.tar" volume > /dev/null
+    docker run --rm -v "$volume":/volume -v "$backup_dir":/backup ubuntu tar cvf "backup/$volume.tar" volume > /dev/null
 
     echo "$(date +"%m/%d/%Y %H:%M:%S"): Backup of volume '$volume_name' completed."
 done
 
-echo "$(date +"%m/%d/%Y %H:%M:%S"): All volumes backed up to '$backup_volume_dir'."
+echo "$(date +"%m/%d/%Y %H:%M:%S"): All volumes backed up to '$backup_dir'."
 
 # Cleanup function
 cleanup() {
     # Get the dates to keep for volumes
-    keep_dates=$(get_last_three_sundays; get_last_three_months; ls -dt $backup_parent_dir/* | head -n 3)
+    keep_dates=$(get_last_three_sundays; get_last_three_months; ls -dt $docker_vol_backup_parent_dir/* | head -n 3)
 
     # Loop through all backup directories for volumes
-    for dir in "$backup_parent_dir"/*; do
+    for dir in "$docker_vol_backup_parent_dir"/*; do
         # Get the date from the directory name
         dir_date=$(basename "$dir")
 
@@ -79,7 +57,4 @@ cleanup() {
 }
 
 # Run the cleanup function
-#cleanup
-
-# rclone sync /backup b2:STEREOV-SERVER-BACKUP
-# echo "$(date +"%m/%d/%Y %H:%M:%S"): Sync /backup to b2:STEREOV-SERVER-BACKUP/backup completed."
+cleanup
